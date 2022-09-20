fb2335e8b151034acf1362981f62bac845e58fec
smford <s.m.ford@gmail.com>
2022-09-20 13:33:03 +0100

    v0.0.4
    - added status and statusall outputs
    - cleaned up verbosity

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 )
 
 const applicationName string = "tasmota-proxy"
-const applicationVersion = "v0.0.3"
+const applicationVersion = "v0.0.4"
 const applicationUrl string = "https://github.com/smford/tasmota-proxy"
 
 var (
@@ -27,12 +27,148 @@ var (
 	homeDirName string
 
 	commandList = map[string]string{
-		"on":     `Power%20On`,
-		"off":    `Power%20Off`,
-		"status": `Status0`,
+		"on":        `Power%20On`,
+		"off":       `Power%20Off`,
+		"status":    `Status0`,
+		"statusall": `Status0`,
 	}
 )
 
+// structure of responses to status
+type StatusResponse struct {
+	Status struct {
+		Module       int      `json:"Module"`
+		DeviceName   string   `json:"DeviceName"`
+		FriendlyName []string `json:"FriendlyName"`
+		Topic        string   `json:"Topic"`
+		ButtonTopic  string   `json:"ButtonTopic"`
+		Power        int      `json:"Power"`
+		PowerOnState int      `json:"PowerOnState"`
+		LedState     int      `json:"LedState"`
+		LedMask      string   `json:"LedMask"`
+		SaveData     int      `json:"SaveData"`
+		SaveState    int      `json:"SaveState"`
+		SwitchTopic  string   `json:"SwitchTopic"`
+		SwitchMode   []int    `json:"SwitchMode"`
+		ButtonRetain int      `json:"ButtonRetain"`
+		SwitchRetain int      `json:"SwitchRetain"`
+		SensorRetain int      `json:"SensorRetain"`
+		PowerRetain  int      `json:"PowerRetain"`
+		InfoRetain   int      `json:"InfoRetain"`
+		StateRetain  int      `json:"StateRetain"`
+	} `json:"Status"`
+	StatusPRM struct {
+		Baudrate      int    `json:"Baudrate"`
+		SerialConfig  string `json:"SerialConfig"`
+		GroupTopic    string `json:"GroupTopic"`
+		OtaURL        string `json:"OtaUrl"`
+		RestartReason string `json:"RestartReason"`
+		Uptime        string `json:"Uptime"`
+		StartupUTC    string `json:"StartupUTC"`
+		Sleep         int    `json:"Sleep"`
+		CfgHolder     int    `json:"CfgHolder"`
+		BootCount     int    `json:"BootCount"`
+		BCResetTime   string `json:"BCResetTime"`
+		SaveCount     int    `json:"SaveCount"`
+		SaveAddress   string `json:"SaveAddress"`
+	} `json:"StatusPRM"`
+	StatusFWR struct {
+		Version       string `json:"Version"`
+		BuildDateTime string `json:"BuildDateTime"`
+		Boot          int    `json:"Boot"`
+		Core          string `json:"Core"`
+		Sdk           string `json:"SDK"`
+		CPUFrequency  int    `json:"CpuFrequency"`
+		Hardware      string `json:"Hardware"`
+		Cr            string `json:"CR"`
+	} `json:"StatusFWR"`
+	StatusLOG struct {
+		SerialLog  int      `json:"SerialLog"`
+		WebLog     int      `json:"WebLog"`
+		MqttLog    int      `json:"MqttLog"`
+		SysLog     int      `json:"SysLog"`
+		LogHost    string   `json:"LogHost"`
+		LogPort    int      `json:"LogPort"`
+		SSID       []string `json:"SSId"`
+		TelePeriod int      `json:"TelePeriod"`
+		Resolution string   `json:"Resolution"`
+		SetOption  []string `json:"SetOption"`
+	} `json:"StatusLOG"`
+	StatusMEM struct {
+		ProgramSize      int      `json:"ProgramSize"`
+		Free             int      `json:"Free"`
+		Heap             int      `json:"Heap"`
+		ProgramFlashSize int      `json:"ProgramFlashSize"`
+		FlashSize        int      `json:"FlashSize"`
+		FlashChipID      string   `json:"FlashChipId"`
+		FlashFrequency   int      `json:"FlashFrequency"`
+		FlashMode        int      `json:"FlashMode"`
+		Features         []string `json:"Features"`
+		Drivers          string   `json:"Drivers"`
+		Sensors          string   `json:"Sensors"`
+	} `json:"StatusMEM"`
+	StatusNET struct {
+		Hostname   string  `json:"Hostname"`
+		IPAddress  string  `json:"IPAddress"`
+		Gateway    string  `json:"Gateway"`
+		Subnetmask string  `json:"Subnetmask"`
+		DNSServer1 string  `json:"DNSServer1"`
+		DNSServer2 string  `json:"DNSServer2"`
+		Mac        string  `json:"Mac"`
+		Webserver  int     `json:"Webserver"`
+		HTTPAPI    int     `json:"HTTP_API"`
+		WifiConfig int     `json:"WifiConfig"`
+		WifiPower  float64 `json:"WifiPower"`
+	} `json:"StatusNET"`
+	StatusMQT struct {
+		MqttHost       string `json:"MqttHost"`
+		MqttPort       int    `json:"MqttPort"`
+		MqttClientMask string `json:"MqttClientMask"`
+		MqttClient     string `json:"MqttClient"`
+		MqttUser       string `json:"MqttUser"`
+		MqttCount      int    `json:"MqttCount"`
+		MaxPacketSize  int    `json:"MAX_PACKET_SIZE"`
+		Keepalive      int    `json:"KEEPALIVE"`
+		SocketTimeout  int    `json:"SOCKET_TIMEOUT"`
+	} `json:"StatusMQT"`
+	StatusTIM struct {
+		Utc      string `json:"UTC"`
+		Local    string `json:"Local"`
+		StartDST string `json:"StartDST"`
+		EndDST   string `json:"EndDST"`
+		Timezone int    `json:"Timezone"`
+		Sunrise  string `json:"Sunrise"`
+		Sunset   string `json:"Sunset"`
+	} `json:"StatusTIM"`
+	StatusSNS struct {
+		Time    string `json:"Time"`
+		Switch1 string `json:"Switch1"`
+	} `json:"StatusSNS"`
+	StatusSTS struct {
+		Time      string `json:"Time"`
+		Uptime    string `json:"Uptime"`
+		UptimeSec int    `json:"UptimeSec"`
+		Heap      int    `json:"Heap"`
+		SleepMode string `json:"SleepMode"`
+		Sleep     int    `json:"Sleep"`
+		LoadAvg   int    `json:"LoadAvg"`
+		MqttCount int    `json:"MqttCount"`
+		Power     string `json:"POWER"`
+		Wifi      struct {
+			Ap        int    `json:"AP"`
+			SSID      string `json:"SSId"`
+			BSSID     string `json:"BSSId"`
+			Channel   int    `json:"Channel"`
+			Mode      string `json:"Mode"`
+			Rssi      int    `json:"RSSI"`
+			Signal    int    `json:"Signal"`
+			LinkCount int    `json:"LinkCount"`
+			Downtime  string `json:"Downtime"`
+		} `json:"Wifi"`
+	} `json:"StatusSTS"`
+}
+
+// structure of responses from poweron, poweroff
 type PowerResponse struct {
 	Power string `json:"POWER"`
 }
@@ -50,6 +186,9 @@ func init() {
 	flag.String("ip", "", "IP")
 	flag.Bool("list", false, "List Devices")
 	flag.Bool("version", false, "Version")
+
+	// temp
+	flag.Bool("verbose", false, "Be verbose")
 
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
@@ -102,7 +241,7 @@ func init() {
 
 func main() {
 	// temp
-	verbose = true
+	verbose = viper.GetBool("verbose")
 
 	if !viper.IsSet("cmd") {
 		fmt.Println("Error, no command defined")
@@ -113,7 +252,9 @@ func main() {
 
 	// check if device is valid
 	if checkDeviceValid(viper.GetString("device")) {
-		fmt.Printf("Device: %s found\n", viper.GetString("device"))
+		if verbose {
+			fmt.Printf("Device: %s found\n", viper.GetString("device"))
+		}
 	} else {
 		fmt.Printf("Device: %s not found\n", viper.GetString("device"))
 		os.Exit(1)
@@ -144,11 +285,35 @@ func main() {
 			res := PowerResponse{}
 			err := json.Unmarshal(response, &res)
 			checkErr(err)
-			fmt.Printf("Prettyprint:\n%s\n--\n", prettyPrint(res))
-			fmt.Printf("State: %s\n", res.Power)
+			fmt.Printf("%s:%s\n", viper.GetString("device"), res.Power)
 		}
 
-		// if status
+		// if status or statusall
+		if strings.EqualFold(cleanCommand, "status") || strings.EqualFold(cleanCommand, "statusall") {
+			res := StatusResponse{}
+			err := json.Unmarshal(response, &res)
+			checkErr(err)
+
+			if strings.EqualFold(cleanCommand, "status") {
+				var powerState string
+				switch res.Status.Power {
+				case 0:
+					powerState = "OFF"
+				case 1:
+					powerState = "ON"
+				default:
+					powerState = "UNKNOWN"
+				}
+
+				fmt.Printf("%s:%s\n", viper.GetString("device"), powerState)
+
+			}
+
+			if strings.EqualFold(cleanCommand, "statusall") {
+				fmt.Printf("%s\n", prettyPrint(res))
+			}
+
+		}
 
 	}
 }
@@ -158,7 +323,9 @@ func sendTasmota(ip string, cmd string) ([]byte, bool) {
 
 	url := fmt.Sprintf("http://%s/cm?cmnd=%s", ip, cmd)
 
-	fmt.Printf("URL: %s\n", url)
+	if verbose {
+		fmt.Printf("URL: %s\n", url)
+	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
